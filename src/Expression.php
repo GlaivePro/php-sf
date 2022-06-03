1b9b60f30f5ad589a87627d0ae79dea81c22a6dc
Džuris <juris@glaive.pro>
2022-06-03 03:27:25 +0300

    Create expression builder

--- src/Expression.php
+++ src/Expression.php
@@ -2,7 +2,64 @@
 
 namespace TontonsB\SF;
 
-class Expression
+class Expression implements \Stringable
 {
-	//
+	public function __construct(
+		public readonly Expression|string $sql,
+		public readonly array $bindings = [],
+	) {}
+
+	public function __toString(): string
+	{
+		return $this->sql;
+	}
+
+	/**
+	 * Ensures $expression is an instance of static. If it's not possible, i.e.
+	 * it's an instance of another class, throws exception.
+	 */
+	public static function make(string|self $expression): static
+	{
+		if ($expression instanceof static)
+			return $expression;
+
+		if (is_string($expression))
+			return new static($expression);
+
+		throw new InvalidExpressionType(
+			'Only strings and '.static::class.' can be wrapped in expressions of type '.static::class.'.'
+		);
+	}
+
+	/**
+	 * Creates a new instance of static that calls $method and supplies $args.
+	 * Any Expression args are merged in the sql expression while raw args
+	 * are moved to bindings and replaced by ?.
+	 */
+	public static function fromMethod(string $method, string|self ...$args): static
+	{
+		$params = [];
+		$bindings = [];
+
+		foreach ($args as $arg) {
+			if ($arg instanceof self) {
+				// Expressions are added to method params as is and
+				// their bindings are appended to the binding array.
+				$params[] = $arg;
+
+				$bindings = [
+					...$bindings,
+					...$arg->bindings
+				];
+			} else {
+				// Raw arguments are replaced by ? and moved to bindings.
+				$params[] = '?';
+				$bindings[] = $arg;
+			}
+		}
+
+		$params = implode(', ', $params);
+
+		return new static("$method($params)", $bindings);
+	}
 }
--- tests/ExpressionTest.php
+++ tests/ExpressionTest.php
@@ -4,13 +4,119 @@ namespace TontonsB\SF\Tests;
 
 use PHPUnit\Framework\TestCase;
 use TontonsB\SF\Expression;
+use TontonsB\SF\Geometry;
+use TontonsB\SF\InvalidExpressionType;
 
 class ExpressionTest extends TestCase
 {
-	public function testInstantiation()
+	public function testConstructor(): void
 	{
-		$expression = new Expression;
+		$expression = new Expression('sql');
 
-		$this->assertIsObject($expression);
+		$this->assertEquals('sql', $expression->sql);
+		$this->assertEquals([], $expression->bindings);
+	}
+
+	public function testConstructorWithBindings(): void
+	{
+		$expression = new Expression('sql', [1, 2]);
+
+		$this->assertEquals('sql', $expression->sql);
+		$this->assertEquals([1, 2], $expression->bindings);
+	}
+
+	public function testStringability(): void
+	{
+		$sql = 'somefun(?)';
+
+		$expression = new Expression($sql, [3, 5]);
+
+		$this->assertEquals($sql, (string) $expression);
+	}
+
+	public function testFactory(): void
+	{
+		$sql = 'somestatement';
+
+		$expr = Expression::make($sql);
+		$this->assertEquals($sql, (string) $expr);
+
+		$expr2 = Expression::make($expr);
+		$this->assertSame($expr, $expr2);
+
+		$expr3 = Expression::make($sql);
+		$this->assertNotSame($expr, $expr3);
+		$this->assertEquals((string) $expr, (string) $expr3);
+	}
+
+	public function testFactoryTypes(): void
+	{
+		$sql = 'somestatement';
+
+		$expr = Expression::make($sql);
+		$this->assertInstanceOf(Expression::class, $expr);
+
+		$geom = Geometry::make($sql);
+		$this->assertInstanceOf(Geometry::class, $geom);
+
+		// Same type is ok and returns object itself
+		$expr2 = Expression::make($expr);
+		$this->assertSame($expr, $expr2);
+
+		$geom2 = Geometry::make($geom);
+		$this->assertSame($geom, $geom2);
+
+		// Subclasses are fine as well and returns object itself:
+		// Every Geometry is still an Expression.
+		$expr3 = Expression::make($geom);
+		$this->assertSame($geom, $expr3);
+
+		// Superclasses are not ok: not every Expression can be a Geometry.
+		$this->expectException(InvalidExpressionType::class);
+		Geometry::make($expr);
+	}
+
+	public function testMethodFactory(): void
+	{
+		$method = 'method';
+
+		$rawArg = 'r1';
+
+		$exprSql = 'expr(?, ?)';
+		$exprBindings = ['a', 'b'];
+		$exprWithBindings = new Expression($exprSql, $exprBindings);
+
+		$geomSql = 'geom';
+		$geomWithoutBindings = new Geometry($geomSql);
+
+		$rawArg2 = 'r2';
+
+		$expression = Expression::fromMethod(
+			'method',
+			$rawArg,
+			$exprWithBindings,
+			$geomWithoutBindings,
+			$rawArg2,
+		);
+
+		$this->assertInstanceOf(Expression::class, $expression);
+		$this->assertEquals("$method(?, $exprSql, $geomSql, ?)", (string) $expression);
+		$this->assertEquals([
+			$rawArg,
+			...$exprBindings,
+			$rawArg2,
+		], $expression->bindings);
+	}
+
+	public function testMethodFactoryTypes(): void
+	{
+		// Any instance of Expression or subclass should always be acceptable
+		// even on children.
+		$expr = new Expression('expr');
+
+		$geometry = Geometry::fromMethod('method', $expr);
+
+		$this->assertInstanceOf(Geometry::class, $geometry);
+		$this->assertEquals('method(expr)', (string) $geometry);
 	}
 }

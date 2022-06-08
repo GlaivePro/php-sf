9be5f83cb09fc7887bdc4fc737eda4dfab04bd54
Džuris <juris@glaive.pro>
2022-06-08 20:32:01 +0300

    Add PostGIS point constructors

---
+++ src/PostGIS/Sfc.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace TontonsB\SF\PostGIS;
+
+use TontonsB\SF\OGC\Sfc as OGCSfc;
+
+/**
+ * Implements constructors available in PostGIS.
+ *
+ * Note that some of these functions may also be available in other DBs, but
+ * the usage may be different. See
+ * https://desktop.arcgis.com/en/arcmap/latest/manage-data/using-sql-with-gdbs/constructor-functions.htm
+ */
+
+// TODO: This is nowhere near completed :)
+
+class Sfc extends OGCSfc
+{
+	/**
+	 * Calls `fromMethod` on the desired class.
+	 *
+	 * We have to specify PostGIS-specific classes here.
+	 */
+	protected static function callFromMethod(string $method, array $args): Geometry
+	{
+		$class = match($method) {
+			'geometry' => Geometry::class,
+			'point' => Point::class,
+			// 'curve' => Curve::class,
+			// 'surface' => Surface::class,
+			// 'geometryCollection' => GeometryCollection::class,
+		};
+
+		return $class::fromMethod(...$args);
+	}
+
+	/**
+	 * Create a Point.
+	 *
+	 * Unlike ST_MakePoint in PostGIS, this also accepts
+	 * makePoint(x: $x, y: $y, m: $m) and will use ST_MakePointM in such case.
+	 *
+	 * If the optional args are omitted, we will also omit them.
+	 */
+	public static function makePoint(float $x, float $y, float $z = null, float $m = null): Point
+	{
+		if (is_null($z) && is_null($m))
+			return Point::fromMethod('ST_MakePoint', $x, $y);
+
+		if (is_null($m))
+			return Point::fromMethod('ST_MakePoint', $x, $y, $z);
+
+		if (is_null($z))
+			return static::makePointM($x, $y, $m);
+
+		return Point::fromMethod('ST_MakePoint', $x, $y, $z, $m);
+	}
+
+	/**
+	 * Create a Point with the measure coordinate.
+	 */
+	public static function makePointM(float $x, float $y, float $m): Point
+	{
+		return Point::fromMethod('ST_MakePointM', $x, $y, $m);
+	}
+
+	/**
+	 * Create a Point.
+	 *
+	 * Unlike ST_Point in PostGIS, this also accepts point($x, $y, m: $m),
+	 * point($x, $y, z: $z) and other combinations and will select the appropriate
+	 * method among ST_Point, ST_PointZ, ST_PointM, ST_PointZM.
+	 *
+	 * If SRID is omitted, we will also omit it.
+	 */
+	public static function point(float $x, float $y, int $srid = null, float $z = null, float $m = null): Point
+	{
+		if (is_null($z) && is_null($m))
+			return is_null($srid)
+				? Point::fromMethod('ST_Point', $x, $y)
+				: Point::fromMethod('ST_Point', $x, $y, $srid);
+
+		if (is_null($m))
+			return static::pointZ($x, $y, $z, $srid);
+
+		if (is_null($z))
+			return static::pointM($x, $y, $m, $srid);
+
+		return static::pointZM($x, $y, $z, $m, $srid);
+	}
+
+	/**
+	 * Create a Point with X, Y and Z.
+	 *
+	 * If SRID is omitted, we will also omit it.
+	 */
+	public static function pointZ(float $x, float $y, float $z, int $srid = null): Point
+	{
+		return is_null($srid)
+			? Point::fromMethod('ST_PointZ', $x, $y, $z)
+			: Point::fromMethod('ST_PointZ', $x, $y, $z, $srid);
+	}
+
+	/**
+	 * Create a Point with X, Y and M.
+	 *
+	 * If SRID is omitted, we will also omit it.
+	 */
+	public static function pointM(float $x, float $y, float $m, int $srid = null): Point
+	{
+		return is_null($srid)
+			? Point::fromMethod('ST_PointM', $x, $y, $m)
+			: Point::fromMethod('ST_PointM', $x, $y, $m, $srid);
+	}
+
+	/**
+	 * Create a Point with X, Y, Z and M.
+	 *
+	 * If SRID is omitted, we will also omit it.
+	 */
+	public static function pointZM(float $x, float $y, float $z, float $m, int $srid = null): Point
+	{
+		return is_null($srid)
+			? Point::fromMethod('ST_PointZM', $x, $y, $z, $m)
+			: Point::fromMethod('ST_PointZM', $x, $y, $z, $m, $srid);
+	}
+}
--- tests/PostGISTest.php
+++ tests/PostGISTest.php
@@ -4,6 +4,8 @@ namespace Tontonsb\SF\Tests;
 
 use PHPUnit\Framework\TestCase;
 use TontonsB\SF\PostGIS\Geometry;
+use TontonsB\SF\PostGIS\Point;
+use TontonsB\SF\PostGIS\Sfc;
 
 class PostGISTest extends TestCase
 {
@@ -18,5 +20,140 @@ class PostGISTest extends TestCase
 		);
 		$this->assertEquals([4326], $setSRID->bindings);
 		$this->assertInstanceOf(Geometry::class, $setSRID);
+	}
+
+	/**
+	 * Ensure that the proxied methods create PostGIS objects
+	 */
+	public function testProxiedConstructors(): void
+	{
+		$this->assertInstanceOf(Geometry::class, Sfc::geomFromText('text'));
+
+		$this->assertInstanceOf(\TontonsB\SF\PostGIS\Point::class, Sfc::pointFromWKB('binary'));
+	}
+
+	public function testMakePointConstructors(): void
+	{
+		$this->assertSame(
+			'ST_MakePoint(?, ?)',
+			(string) Sfc::makePoint(1, 3),
+		);
+
+		$point = Sfc::makePoint(1, 3, 4, 8);
+		$this->assertSame(
+			'ST_MakePoint(?, ?, ?, ?)',
+			(string) $point,
+		);
+		$this->assertEquals([1, 3, 4, 8], $point->bindings);
+
+		$this->assertSame(
+			'ST_MakePoint(?, ?, ?)',
+			(string) Sfc::makePoint(1, 3, 6),
+		);
+
+		$mixedOrderPoint = Sfc::makePoint(y: 1, z: 3, x: 6);
+		$this->assertSame(
+			'ST_MakePoint(?, ?, ?)',
+			(string) $mixedOrderPoint,
+		);
+		$this->assertEquals([6, 1, 3], $mixedOrderPoint->bindings);
+
+		$this->assertSame(
+			'ST_MakePoint(?, ?, ?)',
+			(string) Sfc::makePoint(1, 3, z: 6),
+		);
+
+		$pointM = Sfc::makePoint(1, 3, m: 6);
+		$this->assertSame(
+			'ST_MakePointM(?, ?, ?)',
+			(string) $pointM,
+		);
+		$this->assertEquals([1, 3, 6], $pointM->bindings);
+
+		$pointMM = Sfc::makePointM(1, 3, 6);
+		$this->assertSame(
+			'ST_MakePointM(?, ?, ?)',
+			(string) $pointMM,
+		);
+		$this->assertEquals([1, 3, 6], $pointMM->bindings);
+	}
+
+	public function testPointConstructors(): void
+	{
+		$point = Sfc::point(1, 3);
+		$this->assertSame(
+			'ST_Point(?, ?)',
+			(string) $point,
+		);
+		$this->assertEquals([1, 3], $point->bindings);
+
+		$pointWithSrid = Sfc::point(1, 3, 4326);
+		$this->assertSame(
+			'ST_Point(?, ?, ?)',
+			(string) $pointWithSrid,
+		);
+		$this->assertEquals([1, 3, 4326], $pointWithSrid->bindings);
+
+		$pointM = Sfc::point(1, 3, m: 8, srid: 4326);
+		$this->assertSame(
+			'ST_PointM(?, ?, ?, ?)',
+			(string) $pointM,
+		);
+		$this->assertEquals([1, 3, 8, 4326], $pointM->bindings);
+
+		$this->assertSame(
+			'ST_PointZ(?, ?, ?)',
+			(string) Sfc::point(1, 3, z: 4),
+		);
+
+		$this->assertSame(
+			'ST_PointZ(?, ?, ?, ?)',
+			(string) Sfc::point(1, 3, 4326, 8),
+		);
+
+		$this->assertSame(
+			'ST_PointM(?, ?, ?)',
+			(string) Sfc::point(1, 3, m: 8),
+		);
+
+		$this->assertSame(
+			'ST_PointZM(?, ?, ?, ?)',
+			(string) Sfc::point(1, 3, z: 4, m: 8),
+		);
+
+		$this->assertSame(
+			'ST_PointZM(?, ?, ?, ?, ?)',
+			(string) Sfc::point(1, 3, z: 4, m: 8, srid: 4326),
+		);
+
+		$this->assertSame(
+			'ST_PointZ(?, ?, ?)',
+			(string) Sfc::pointZ(1, 3, 4),
+		);
+
+		$this->assertSame(
+			'ST_PointZ(?, ?, ?, ?)',
+			(string) Sfc::pointZ(1, 3, 4, 4326),
+		);
+
+		$this->assertSame(
+			'ST_PointM(?, ?, ?)',
+			(string) Sfc::pointM(1, 3, 8),
+		);
+
+		$this->assertSame(
+			'ST_PointM(?, ?, ?, ?)',
+			(string) Sfc::pointM(1, 3, 8, 4326),
+		);
+
+		$this->assertSame(
+			'ST_PointZM(?, ?, ?, ?)',
+			(string) Sfc::pointZM(1, 3, 4, 8),
+		);
+
+		$this->assertSame(
+			'ST_PointZM(?, ?, ?, ?, ?)',
+			(string) Sfc::pointZM(1, 3, 4, 8, 4326),
+		);
 	}
 }

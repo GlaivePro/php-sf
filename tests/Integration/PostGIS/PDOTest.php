5ce406c4ce392379da8fcd74b6c028e856bca962
Džuris <juris@glaive.pro>
2023-03-11 23:22:21 +0200

    Add more integration testing

--- tests/Integration/PostGIS/PDOTest.php
+++ tests/Integration/PostGIS/PDOTest.php
@@ -3,6 +3,8 @@
 namespace Janaseta\SF\Tests\Integration\PostGIS;
 
 use Janaseta\SF\Expression;
+use Janaseta\SF\PostGIS\Geometry;
+use Janaseta\SF\PostGIS\Point;
 use Janaseta\SF\PostGIS\Sfc;
 use PDO;
 use PHPUnit\Framework\TestCase;
@@ -31,6 +33,54 @@ class PDOTest extends TestCase
 		$this->assertEquals(56, $this->selectVal($point->y()));
 		$this->assertEquals(4326, $this->selectVal($point->srid()));
 	}
+
+	/**
+	 * Let's produce a triangular hull around some points and check whether
+	 * other geometries are inside the hull.
+	 */
+	public function testComplexActions(): void
+	{
+		// Let's try various APIs
+		$points = [
+			Sfc::makePoint(0, 0),
+			new Point('ST_MakePoint(?, ?)', [0, 10]),
+			new Geometry("'POINT(5 5)'::geometry"),
+		];
+
+		$hull = $points[0]->union($points[1])->union($points[2])->convexHull();
+
+		$this->assertSame('POLYGON((0 0,0 10,5 5,0 0))', $this->selectVal($hull->asText()));
+
+		$pointInside = Sfc::point(3, 5);
+		$pointOutside = Sfc::point(10, 10);
+		$this->assertTrue($this->selectVal(
+			$hull->contains($pointInside)
+		));
+		$this->assertTrue($this->selectVal(
+			$pointInside->within($hull)
+		));
+		$this->assertFalse($this->selectVal(
+			$hull->contains($pointOutside)
+		));
+		$this->assertFalse($this->selectVal(
+			$pointOutside->within($hull)
+		));
+
+		$line = Sfc::lineFromText('LINESTRING(3 3,10 10)');
+		$this->assertFalse($this->selectVal(
+			$hull->contains($line)
+		));
+		$this->assertFalse($this->selectVal(
+			$line->within($hull)
+		));
+		$this->assertTrue($this->selectVal(
+			$hull->intersects($line)
+		));
+		$this->assertTrue($this->selectVal(
+			$line->intersects($hull)
+		));
+	}
+
 
 	protected function selectVal(Expression $expression)
 	{
